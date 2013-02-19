2979655f5ed3548a154845d941620e679cc64466
Jake Levine <levine.jake@gmail.com>
2013-02-19 00:05:21 -0500

    updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :tumblr, '0GMVFegjf6D0cyWTCP6ilhgYwokPRh3Fgy8TvA4mMcez6pmhiJ', 'G6IHgthYTBmMyupTGO1yxQlmb7c91s6f9sONOakZuejyfayxlA'
+  #on local
+  #provider :tumblr, '0GMVFegjf6D0cyWTCP6ilhgYwokPRh3Fgy8TvA4mMcez6pmhiJ', 'G6IHgthYTBmMyupTGO1yxQlmb7c91s6f9sONOakZuejyfayxlA'
+  
+  #on heroku
+  provider :tumblr, 'ZozRvHImkaTAbHr2VKeQ4cMlzAmI77wjHc4gRT2CMzMDoatBqj', 'y4JJuYmOma0BaFNKHeJYSlIJ4bY2UeZiwjIDt6LxbvHPrynUyI'
   provider :twitter, 'U6yihUf26u2039yK1eNjSg', '6sfCeo32sT9zk5bTgFbjzYwNxKpTK62lEGl6nNy8wo'
 
 end
